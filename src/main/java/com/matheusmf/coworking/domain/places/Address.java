4d95df351e66915344e1ef53ccf61ff95093ef68
Matheus Farias <mfarias@mmcafe.com.br>
2021-03-03 13:51:50 -0300

    add address in places

--- src/main/java/com/matheusmf/coworking/domain/places/Address.java
+++ src/main/java/com/matheusmf/coworking/domain/places/Address.java
@@ -12,4 +12,19 @@ public class Address {
     private String city;
     private String street;
     private String complement;
+
+    public Address(String country, String state, String city, String street, String complement) {
+        if (country == null || country.isEmpty()
+                || state == null || state.isEmpty()
+                || city == null || city.isEmpty()
+                || street == null || street.isEmpty()) {
+            throw new IllegalArgumentException("Country, State, City and Street are required!");
+        }
+
+        this.country = country;
+        this.state = state;
+        this.city = city;
+        this.street = street;
+        this.complement = complement;
+    }
 }
